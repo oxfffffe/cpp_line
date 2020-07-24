cc04d060078427fb1ec51f0ca6f5238e0518df09
oxfffffe <59066701+oxfffffe@users.noreply.github.com>
2020-07-25 00:28:00 +0300

    Update regex.cpp

--- regex.cpp
+++ regex.cpp
@@ -43,13 +43,13 @@ bool lightning::regex::matchGroup(int ch, int group)
 {
 	if ((group & 0xff) == '.')
 	{
-		group ^= 0x100; /* get "mask" */
+		group ^= 0x100;
 	}
 	if (group < 0x100)
 	{
 		return ch == group;
 	}
-	switch (group & 0xff)
+	switch (group & 0xff) /* get "mask" if group < 255 */
 	{
 		case '.': return  true;
 		case 'd': return  isdigit(ch);
