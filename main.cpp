979d23e41f9926e9d1483b527c87de5b3f22b536
oxfffffe <59066701+oxfffffe@users.noreply.github.com>
2020-07-27 14:46:51 +0200

    memory allocator

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using lightning::Allocator;
 int main(void)
 {
 #ifdef	__START_TIME
-		__START_TIME
+	__START_TIME
 #endif
 #ifdef	__DEBUG_LINE
 	Line a = "[0] !//[regex]";
@@ -34,6 +34,6 @@ int main(void)
 	std::cout << regex::regex_find(pattern, "\\S+\\s\\S+\\s\\S+") << std::endl;
 #endif//__DEBUG_REGEX
 #ifdef	__FINISHED_AT
-		__FINISHED_AT
+	__FINISHED_AT
 #endif
 }
