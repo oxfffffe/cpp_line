40450db9c73df7a62d0af37c51601558f5538cef
oxfffffe <59066701+oxfffffe@users.noreply.github.com>
2020-07-30 17:33:37 +0200

    Update hash.hpp

--- src/Lightning/Hash/hash.hpp
+++ src/Lightning/Hash/hash.hpp
@@ -2,18 +2,25 @@
 #define __HASH_H
 
 #include "src/Lightning/namespace/lightning.hpp"
-#include <stdint.h>
 
 /*!
- * @brief Hash represents fast comparison of
+ * @brief Hash class represents fast comparison of
  * two strings using CRC32 algorithm
  */
 class lightning::Hash
 {
 public:
-	static uint32_t crc32gen(const char *);
+	static long crc32gen(const char*);
+
+	static short crc8gen(const char*);
 
 	static bool crc32cmp(const char*, const char*);
+
+	static bool crc8cmp(const char*, const char*);
+
+private:
+	static const long crc32_lookup_table[256];
+	static const short crc8_lookup_table[256];
 };
 
 #endif // __HASH_H
