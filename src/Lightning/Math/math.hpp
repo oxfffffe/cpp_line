0464310f865ccb8d71338b82e698b8a1d6a0602a
oxfffffe <59066701+oxfffffe@users.noreply.github.com>
2020-08-09 19:25:02 +0200

    Update math.hpp

--- src/Lightning/Math/math.hpp
+++ src/Lightning/Math/math.hpp
@@ -6,22 +6,37 @@
 class lightning::Math {
 public:
 	template<typename T>
-	static auto max(const T expr1, const T expr2) -> T;
+	static auto power(const T expr, const short extent) noexcept -> T;
 
 	template<typename T>
-	static auto min(const T expr1, const T expr2) -> T;
+	static auto fabs(const T expr) noexcept -> T;
 
 	template<typename T>
-	static auto roundDown(const T expr) -> T;
+	static auto max(const T expr1, const T expr2) noexcept -> T;
 
 	template<typename T>
-	static auto roundUp(const T expr) -> T;
+	static auto min(const T expr1, const T expr2) noexcept -> T;
 
 	template<typename T>
-	static auto squareRoot(const T expr) -> T;
+	static auto roundDown(const T expr) noexcept -> T;
 
 	template<typename T>
-	static auto power(const T expr, const short extent) -> T;
+	static auto roundUp(const T expr) noexcept -> T;
+
+	template<typename T>
+	static auto squareRoot(const T expr) noexcept -> T;
+
+	template<typename T>
+	static auto sin(const T expr) noexcept -> T;
+
+	template<typename T>
+	static auto cos(const T expr) noexcept -> T;
+
+	template<typename T>
+	static auto tg(const T expr) noexcept -> T;
+
+	template<typename T>
+	static auto ctg(const T expr) noexcept -> T;
 };
 
 #include "math.tcc"
